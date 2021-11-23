7a7511324c913f0e17ab76d3ee6601ffa003a042
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-24 01:50:57 +0600

    Get Request
    5

--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { student, Student } = require('../models/students');
+const { Student } = require('../models/students');
 const router = express.Router();
 
 
@@ -24,8 +24,15 @@ const newStudent = async (req, res) => {
 
 }
 
-const studentDetail = (req, res) => {
-    const id = parseInt(req.params.id);
+const studentDetail = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const student = await Student.findById(id);
+        if (!student) return res.status(404).send('ID not found!');
+        res.send(student);
+    } catch (err) {
+        return res.status(404).send('ID not found!');
+    }
 
 }
 
