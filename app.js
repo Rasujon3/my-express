e955c0e8a3c5eb09453db2eba20cdd466ac5938c
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-24 01:30:33 +0600

    Structuring Project
    1

--- app.js
+++ app.js
@@ -16,39 +16,17 @@ mongoose.connect('mongodb://localhost:27017/my-student-2', {
 
 app.use(express.json()); // post/put/patch => json obj =>req.body
 
-app.use(express.urlencoded({ extended: true })); // id = 1 & name = sujon
-
-app.use(express.static('public'));
-
-app.use(morgan('short'));
-
-app.use((req, res, next) => {
-    console.log("I am middleware 1!");
-    next();
-});
-app.use((req, res, next) => {
-    console.log("I am middleware 2!");
-    next();
-});
 app.use('/api/students', studentRouter);
 
-app.get('/about', () => { });
-
-app.get('/', (req, res, next) => {
-    // res.send("Another Response!");
-    next();
+app.get('/', (req, res) => {
+    res.send("Hello from express js!");
 });
-
-app.get('/', (request, response) => {
-    console.log("I am get Request Middleware!");
-    response.send("Hello from express js!");
-});
-
-
-
-
 
 const port = 5000;
 app.listen(port, () => {
     console.log(`Listing on port ${port}...`);
 })
+
+// Mongooose -> Model -> Collection
+// Import Model
+// Connect Database
--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -1,74 +1,34 @@
 const express = require('express');
-const { student } = require('../models/students');
+const { student, Student } = require('../models/students');
 const router = express.Router();
-const db = require('../db');
 
 
-const studentList = (req, res) => {
-    db.getDbStudents()
-        .then(students => {
-            res.send(students);
-        })
+const studentList = async (req, res) => {
+    const students = await Student.find()
+        .sort({ name: 1 });
+    res.send(students);
 }
 
 const newStudent = (req, res) => {
     const student = req.body;
-    db.getDbStudents()
-        .then(students => {
-            students.push(student);
-            db.insertDbStudent(students)
-                .then(data => {
-                    res.send(student);
 
-                })
-        })
 }
 
 const studentDetail = (req, res) => {
     const id = parseInt(req.params.id);
-    db.getDbStudents()
-        .then(students => {
-            const student = students.find(s => s.id === id);
-            if (!student) res.status(404).send("No student found with this id!");
-            else res.send(student);
-        });
+
 }
 
 const studentUpdate = (req, res) => {
     const id = parseInt(req.params.id);
     const updatedData = req.body;
-    db.getDbStudents()
-        .then(students => {
-            const student = students.find(s => s.id === id);
-            if (!student) res.status(404).send("No student found with this id!");
-            else {
-                const i = students.findIndex(s => s.id === id);
-                students[i] = updatedData;
-                db.insertDbStudent(students)
-                    .then(msg => res.send(updatedData));
-            }
-        });
+
 }
 
 const studentDelete = (req, res) => {
     const id = parseInt(req.params.id);
-    db.getDbStudents()
-        .then(students => {
-            const student = students.find(s => s.id === id);
-            if (!student) res.status(404).send("No student found with this id!");
-            const updatedStudents = students.filter(s => s.id !== id);
-            db.insertDbStudent(updatedStudents)
-                .then(msg => res.send(student))
-        });
+
 }
-
-// app.get('/another', (request, response) => {
-//     response.send("I am another response!");
-// })
-
-// app.get('/students', (request, response) => {
-//     response.send(JSON.stringify(["Rahim", "Karim"]));
-// })
 
 router.route('/')
     .get(studentList)
